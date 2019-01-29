e4899a8f499d09f4c7623ab694c5da4ed02af2eb
akshay.pokley <akshay85pokley@gmail.com>
2019-01-29 16:47:16 +0530

    15012019

--- src/test/java/TestHomePage.java
+++ src/test/java/TestHomePage.java
@@ -1,9 +1,15 @@
+import jxl.write.WritableCellFormat;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.HomePage;
+
+import java.awt.*;
 
 import static Utilities.UtilityDirectory.GetUrl;
 import static Utilities.UtilityDirectory.openBrowser;
@@ -12,42 +18,44 @@ import static Utilities.UtilityDirectory.openBrowser;
  * Created by akshay.pokley on 1/11/2019.
  */
 public class TestHomePage {
-
-    WebDriver driver;
-    HomePage homePage=new HomePage(driver);
-    @BeforeTest
-    public void BeforeTest()
-    {
-         driver=  openBrowser("chrome");
-         GetUrl("url");
-
-    }
-
-    @AfterTest
-    public void AfterTest()
-    {
-
-    }
-
-    @Test
-    public void HPagetesting()
-    {
-
-        try
-        {
-         String hometitel=homePage.getHometitle().getText();
-         if(hometitel.equals(""))
-         {
-             System.out.println("Title of page same as excepted");
-         }else {
-             System.out.println("Title of page same as Not_excepted");
-         }
-
-
-        }catch (Throwable e)
-        {
-
-        }
-
-    }
+    static WebDriver driver;
+    public Label l4;
+    public static WritableCellFormat cellFormat;
+    public static WritableCellFormat cellFormat1;
+    public static WritableCellFormat cellFormat3;
+    public static WritableCellFormat cellFormat4;
+    public WritableCellFormat cellFormat2;
+    public static WritableCellFormat cellFormat6;
+    public static WritableCellFormat cellFormat5;
+    public String TestCase;
+    public WritableWorkbook writableTempSource;
+    public WritableWorkbook copyDocument;
+    public WritableSheet sourceSheet;
+    public static WritableSheet targetSheet;
+    public Workbook sourceDocument;
+    /*****************************************************************/
+    private static int n = 2;
+    private static int j = 1;
+    public static String Result;
+    public static String k;
+    public static String ResultPass1 = "Username";
+    public static String ResultFail1 = "Password";
+    public static String Actual;
+    public static String Actual2;
+    static int LastRow;
+    static int SetBord;
+    static int RowIncr;
+    static final java.util.regex.Pattern String = java.util.regex.Pattern.compile("^[A-Za-z, ]++$");
+    static final java.util.regex.Pattern Num = java.util.regex.Pattern.compile("^[0-9]++$");
+    static final java.util.regex.Pattern Emailval = java.util.regex.Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+    static final java.util.regex.Pattern Alphnu = java.util.regex.Pattern.compile("^[A-Za-z,0-9  ]++$");
+    static final java.util.regex.Pattern Flot = java.util.regex.Pattern.compile("^[+-]?([0-9]*[.])?[0-9]++$");
+    public static String subWindowHandler = null;
+    public static String Actvalue;
+    public static int x=3;
+    public static int u;
+    public static int vb=2;
+    public static int k1;
+    public static int rr=1;
+    public static int Rec=2;
 }
